e68d92e92582f76d5bc506e54726bc496b94342b
Webymind <webymind.mauritius@gmail.com>
2024-04-19 17:54:36 +0400

    main

--- resources/views/user/reschedule.blade.php
+++ resources/views/user/reschedule.blade.php
@@ -1,4 +1,52 @@
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/favicon-32x32"
+        href="https://user-images.githubusercontent.com/97634238/165641863-c75ce720-db06-4f6a-b212-283017ff5d34.png">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Hreflang Tags -->
+    <link rel="alternate" hreflang="en" href="{{ url('/en') }}" />
+    <link rel="alternate" hreflang="fr" href="{{ url('/fr') }}" />
+    <!-- Canonical URL -->
+    <link rel="canonical" href="{{ url()->current() }}" />
+    <!-- Favicon -->
+    <link rel="icon" type="image/favicon-32x32"
+        href="https://user-images.githubusercontent.com/97634238/165641863-c75ce720-db06-4f6a-b212-283017ff5d34.png">
+
+    <meta name="norton-safeweb-site-verification"
+        content="0budjdmnzmoejh6ii9i2y-f8h5v1z9gncd7t72386vuk9vpb4obp2ejhi09x16qjkxob-n78wu758o-fhkgw7335fvlvmsox3b-gm9f5m9io-8ybpkb9dnhtrgiax1d5">
+
+    <title>LLosteo.com | How to reschedule your Appointment</title>
+<meta name="description"
+        content="Osteopaths in Mauritius, Learn how to reschedule your appointment taken via llosteo.com website.">
+    
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FF8J6VXZK5"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-FF8J6VXZK5');
+    </script>
+
+</head>
+
+<body>
+
 <div style="position:relative; width:100%; height:0px; padding-bottom:56.250%">
     <iframe allow="fullscreen;autoplay" allowfullscreen height="100%" src="https://streamable.com/e/2zgdr1?autoplay=1&muted=1" width="100%" style="border:none; width:100%; height:100%; position:absolute; left:0px; top:0px; overflow:hidden;">
     </iframe>
 </div>
+
+</body>
+
+</html>
